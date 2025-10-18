ea58837790015a909ccf1f6fb11e08a57c842be5
Adrian Zdankowski <adrianzdankowski@gmail.com>
2025-10-18 15:22:27 +0200

    Add airplanes and plane types initialization

--- src/main/java/org/example/configuration/observer/InitializedData.java
+++ src/main/java/org/example/configuration/observer/InitializedData.java
@@ -6,6 +6,10 @@ import jakarta.enterprise.context.control.RequestContextController;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import lombok.SneakyThrows;
+import org.example.airplane.entity.Airplane;
+import org.example.airplane.entity.PlaneType;
+import org.example.airplane.service.AirplaneService;
+import org.example.airplane.service.PlaneTypeService;
 import org.example.pilot.entity.Pilot;
 import org.example.pilot.entity.PilotRank;
 import org.example.pilot.service.PilotService;
@@ -16,6 +20,8 @@ import java.util.UUID;
 @ApplicationScoped
 public class InitializedData {
     private PilotService pilotService;
+    private PlaneTypeService planeTypeService;
+    private AirplaneService airplaneService;
 
     private final RequestContextController requestContextController;
 
@@ -23,9 +29,13 @@ public class InitializedData {
     @Inject
     public InitializedData(
             PilotService pilotService,
+            PlaneTypeService planeTypeService,
+            AirplaneService airplaneService,
             RequestContextController requestContextController
     ) {
         this.pilotService = pilotService;
+        this.planeTypeService = planeTypeService;
+        this.airplaneService = airplaneService;
         this.requestContextController = requestContextController;
     }
 
@@ -69,6 +79,82 @@ public class InitializedData {
         pilotService.create(major);
         pilotService.create(officer);
         pilotService.create(otherPilot);
+
+        PlaneType fighterAircraft = PlaneType.builder()
+                .id(UUID.fromString("63d6f249-48ab-4d71-85bb-34ed3bd1a748"))
+                .name("Fighter")
+                .weight(9.5)
+                .numberOfEngines(1)
+                .build();
+
+        PlaneType passengerAircraft = PlaneType.builder()
+                .id(UUID.fromString("c8e3e5ec-ee6d-4841-8f73-188dcd8a05b8"))
+                .name("Passenger")
+                .weight(100)
+                .numberOfEngines(2)
+                .build();
+
+        PlaneType transportAircraft = PlaneType.builder()
+                .id(UUID.fromString("0c3fd662-b90d-415f-ae0c-af1554fc5e85"))
+                .name("Transport")
+                .weight(70.3)
+                .numberOfEngines(4)
+                .build();
+
+        planeTypeService.create(fighterAircraft);
+        planeTypeService.create(passengerAircraft);
+        planeTypeService.create(transportAircraft);
+
+        Airplane f16 = Airplane.builder()
+                .id(UUID.fromString("59934c21-368b-48fc-905a-2f34575fdff1"))
+                .name("F-16 Fighting Falcon")
+                .yearOfProduction(2022)
+                .flightHours(1245)
+                .planeType(fighterAircraft)
+                .pilot(general)
+                .build();
+
+        Airplane f35 = Airplane.builder()
+                .id(UUID.fromString("78fa23b8-152d-47a1-aa8e-d11aa412aa64"))
+                .name("F-35 Lightning II")
+                .yearOfProduction(2024)
+                .flightHours(600)
+                .planeType(fighterAircraft)
+                .pilot(general)
+                .build();
+
+        Airplane dreamliner = Airplane.builder()
+                .id(UUID.fromString("366830cd-ecb5-4bc6-9d55-60a06a13994f"))
+                .name("Boeing 787")
+                .yearOfProduction(2018)
+                .flightHours(12415)
+                .planeType(passengerAircraft)
+                .pilot(major)
+                .build();
+
+        Airplane airbus = Airplane.builder()
+                .id(UUID.fromString("6fd80b80-00d9-42c9-9888-2b3f168f0dd0"))
+                .name("Airbus A320")
+                .yearOfProduction(2010)
+                .flightHours(13015)
+                .planeType(passengerAircraft)
+                .pilot(officer)
+                .build();
+
+        Airplane c130 = Airplane.builder()
+                .id(UUID.fromString("681bc221-0a21-4f3e-a726-6cebac04b210"))
+                .name("Airbus A320")
+                .yearOfProduction(1995)
+                .flightHours(24542)
+                .planeType(transportAircraft)
+                .pilot(otherPilot)
+                .build();
+
+        airplaneService.create(f16);
+        airplaneService.create(f35);
+        airplaneService.create(dreamliner);
+        airplaneService.create(airbus);
+        airplaneService.create(c130);
 
         requestContextController.deactivate();
     }
